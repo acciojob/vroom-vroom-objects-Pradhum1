881da9004daa8ecf380f4e8bfa51f10c3fc343e9
Pradhum1 <113616448+Pradhum1@users.noreply.github.com>
2024-09-05 14:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	
+		this.make=make;
+		this.model=model;
+	}
+	Car.Prototype.getMakeModel=function(){
+		return `${this.make} ${this.model}`;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	
+	CarCall(this,make,model){
+		this.topSpeed=topSpeed;
+	
+	}
+	SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add a method to the SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
+
 
 // Do not change the code below
 window.Car = Car;
